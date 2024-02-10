b8cde052528577a270f9f4ef9ce581e4f4c984c1
Kurt Duncan <kurtaduncan@gmail.com>
2024-02-10 15:40:30 -0700

    Copyright notices

--- kexec/cmd/packutil.go
+++ kexec/cmd/packutil.go
@@ -1,3 +1,7 @@
+// Khalehla Project
+// Copyright © 2023-2024 by Kurt Duncan, BearSnake LLC
+// All Rights Reserved
+
 package main
 
 import (
--- kexec/consoleMgr/consoleManager.go
+++ kexec/consoleMgr/consoleManager.go
@@ -1,5 +1,5 @@
 // Khalehla Project
-// Copyright © 2023 by Kurt Duncan, BearSnake LLC
+// Copyright © 2023-2024 by Kurt Duncan, BearSnake LLC
 // All Rights Reserved
 
 package consoleMgr
