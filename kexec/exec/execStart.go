745b54e46727ce33155f19cba4dd11104030a6a1
Kurt Duncan <kurtaduncan@gmail.com>
2024-03-23 20:17:01 -0600

    Incremental work on exec initialization

--- kexec/exec/execStart.go
+++ kexec/exec/execStart.go
@@ -5,10 +5,16 @@
 package exec
 
 import (
+	"fmt"
 	"khalehla/kexec"
 	"khalehla/kexec/csi"
 	"khalehla/klog"
 )
+
+// assignSystemLibraryFiles assigns the system library files to the Exec, although NOT exclusively.
+func (e *Exec) assignSystemLibraryFiles() {
+	// TODO
+}
 
 func (e *Exec) callCSI(statement string) uint64 {
 	pcs, _, stat := csi.ParseControlStatement(e.runControlEntry, statement)
@@ -19,6 +25,117 @@ func (e *Exec) callCSI(statement string) uint64 {
 		klog.LogErrorF("Exec", "Status %012o : %v", stat, statement)
 	}
 	return stat
+}
+
+func (e *Exec) copyTapeFile(tapeFileName string, systemFileName string) {
+	// TODO
+}
+
+// loadSystemLibrary loads the three system library files from a library tape.
+// The library tape (in lieu of boot tape) contains the following files in copy,g format
+//
+//	SYS$*LIB$
+//	SYS$*RUN$
+//	SYS$*RLIB$
+func (e *Exec) loadSystemLibrary() {
+	reelNumber := ""
+	for !kexec.IsValidReelNumber(reelNumber) {
+		msg := "Enter reel number of system library tape"
+		reelNumber, _ := e.SendExecReadReplyMessage(msg, 6, nil)
+		if e.stopFlag {
+			return
+		}
+		if kexec.IsValidReelNumber(reelNumber) {
+			break
+		}
+		msg = "Invalid reel number entered"
+		e.SendExecReadOnlyMessage(msg, nil)
+	}
+
+	if e.stopFlag {
+		return
+	}
+
+	stat := e.callCSI(fmt.Sprintf("@ASG,TJ LIB.,%v,%v", e.configuration.TapeDefaultMnemonic, reelNumber))
+	if e.stopFlag {
+		return
+	} else if stat&0_400000_000000 != 0 {
+		klog.LogFatalF("Exec", "Cannot assign tape unit for library tape:%012o", stat)
+		e.Stop(kexec.StopFileAssignErrorOccurredDuringSystemInitialization)
+		return
+	}
+
+	stat = e.callCSI(fmt.Sprintf("@CAT,G SYS$*LIB$(+1),%v/%v/TRK/%v",
+		e.configuration.LibAssignMnemonic,
+		e.configuration.LibInitialReserve,
+		e.configuration.LibMaximumSize))
+	if e.stopFlag {
+		return
+	} else if stat&0_400000_000000 != 0 {
+		klog.LogFatalF("Exec", "Cannot catalog SYS$*LIB$ file:%012o", stat)
+		e.Stop(kexec.StopFileAssignErrorOccurredDuringSystemInitialization)
+		return
+	}
+
+	e.copyTapeFile("LIB", "SYS$*LIB$")
+	if e.stopFlag {
+		return
+	}
+
+	stat = e.callCSI(fmt.Sprintf("@CAT,G SYS$*RUN$(+1),%v/%v/TRK/%v",
+		e.configuration.RunAssignMnemonic,
+		e.configuration.RunInitialReserve,
+		e.configuration.RunMaximumSize))
+	if e.stopFlag {
+		return
+	} else if stat&0_400000_000000 != 0 {
+		klog.LogFatalF("Exec", "Cannot catalog SYS$*RUN$ file:%012o", stat)
+		e.Stop(kexec.StopFileAssignErrorOccurredDuringSystemInitialization)
+		return
+	}
+
+	stat = e.callCSI("@ASG,AX SYS$*RUN$")
+	if e.stopFlag {
+		return
+	} else if stat&0_400000_000000 != 0 {
+		klog.LogFatalF("Exec", "Cannot assign SYS$*RUN$ file:%012o", stat)
+		e.Stop(kexec.StopFileAssignErrorOccurredDuringSystemInitialization)
+		return
+	}
+
+	e.copyTapeFile("LIB", "SYS$*RUN$")
+	if e.stopFlag {
+		return
+	}
+
+	stat = e.callCSI(fmt.Sprintf("@CAT,G SYS$*RLIB$(+1),%v/128/TRK/9999",
+		e.configuration.MassStorageDefaultMnemonic))
+	if e.stopFlag {
+		return
+	} else if stat&0_400000_000000 != 0 {
+		klog.LogFatalF("Exec", "Cannot catalog SYS$*RLIB$ file:%012o", stat)
+		e.Stop(kexec.StopFileAssignErrorOccurredDuringSystemInitialization)
+		return
+	}
+
+	stat = e.callCSI("@ASG,AX SYS$*RLIB$")
+	if e.stopFlag {
+		return
+	} else if stat&0_400000_000000 != 0 {
+		klog.LogFatalF("Exec", "Cannot assign SYS$*RLIB$ file:%012o", stat)
+		e.Stop(kexec.StopFileAssignErrorOccurredDuringSystemInitialization)
+		return
+	}
+
+	e.copyTapeFile("LIB", "SYS$*RLIB$")
+	if e.stopFlag {
+		return
+	}
+
+	e.callCSI("@FREE LIB")
+	e.callCSI("@FREE,X SYS$*LIB$")
+	e.callCSI("@FREE,X SYS$*RUN$")
+	e.callCSI("@FREE,X SYS$*RLIB$")
 }
 
 // performInitialBoot is invoked on the first session initiated by the application.
@@ -33,11 +150,28 @@ func (e *Exec) performInitialBoot() {
 		} else if reply == "N" {
 			e.Stop(kexec.StopConsoleResponseRequiresReboot)
 		}
-	}
 
-	e.mfdMgr.InitializeMassStorage()
-	if e.stopFlag {
-		return
+		e.mfdMgr.InitializeMassStorage()
+		if e.stopFlag {
+			return
+		}
+
+		// Catalog SYS$*DLOC$
+		stat := e.callCSI(fmt.Sprintf("@CAT,G SYS$*DLOC$(+1)/RDKDLC/WRKDLC,%v",
+			e.configuration.DLOCAssignMnemonic))
+		if e.stopFlag {
+			return
+		} else if stat&0_400000_000000 != 0 {
+			klog.LogFatalF("Exec", "Cannot catalog SYS$*DLOC$ file:%012o", stat)
+			e.Stop(kexec.StopFileAssignErrorOccurredDuringSystemInitialization)
+			return
+		}
+
+	} else {
+		e.mfdMgr.RecoverMassStorage()
+		if e.stopFlag {
+			return
+		}
 	}
 
 	stat := e.callCSI("@ASG,AX SYS$*MFD$$")
@@ -46,22 +180,26 @@ func (e *Exec) performInitialBoot() {
 		return
 	}
 
-	// TODO security officer setup
+	// TODO security officer setup, user and account file creation or assignment
 
-	if e.jumpKeys[kexec.JumpKey4Index] {
-		// TODO for JK4, load libraries from library tape
-		// libray tape (in lieu of boot tape) contains the following files in copy,g format...
-		//   SYS$*LIB$
-		//   SYS$*RUN$
-		//   SYS$*RLIB$
+	if e.jumpKeys[kexec.JumpKey4Index] || e.jumpKeys[kexec.JumpKey13Index] {
+		e.loadSystemLibrary()
+		if e.stopFlag {
+			return
+		}
+	}
+
+	e.assignSystemLibraryFiles()
+	if e.stopFlag {
+		return
 	}
 
 	if e.jumpKeys[kexec.JumpKey7Index] {
-		// TODO ask whether to innitialize TIP, etc
+		// TODO ask whether to initialize TIP, etc
 	}
 
 	if e.jumpKeys[kexec.JumpKey9Index] {
-		// TODO ask whether to initialiize or recover GENF$
+		// TODO ask whether to initialize or recover GENF$
 		// (I think this is implied with JK13)
 	}
 }
